0c46e93ca4a8ca4c36a5897161aa804a6f098b1c
juxiezuo-cyh <422738768@qq.com>
2017-08-15 17:04:01 +0800

    详情页面

---
+++ src/pages/detail/count.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>count</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../pages/index'
-
+import DetailPage from '../pages/detail'
+import DetailForPage from '../pages/detail/forecast'
+import DetailCouPage from '../pages/detail/count'
+import DetailAnaPage from '../pages/detail/analysis'
+import DetailPubPage from '../pages/detail/publish'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +14,28 @@ export default new Router({
     {
       path: '/',
       component: IndexPage
+    },
+    {
+      path:'/detail',
+      component: DetailPage,
+      children: [
+        {
+          path:'analysis',
+          component: DetailAnaPage
+        },
+        {
+          path:'count',
+          component: DetailCouPage
+        },
+        {
+          path:'forecast',
+          component: DetailForPage
+        },
+        {
+          path:'publish',
+          component: DetailPubPage
+        }
+      ]
     }
   ]
 })
